78e44af45d20bcfbff5eaf8f11a965c0c7de73e0
ziad <shalabyziad94@gmail.com>
2026-07-04 15:56:56 +0300

    update

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "workout-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
